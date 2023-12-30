1019816f1814ec213d67bd47a2b36cabfc463abe
Serhiy199 <bryklin.best.1994@gmail.com>
2023-12-30 11:11:22 +0200

    one

--- src/main.js
+++ src/main.js
@@ -3,30 +3,29 @@ import 'izitoast/dist/css/iziToast.min.css';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
-const BASE_URL = 'https://pixabay.com/api/';
-const key = 'key=41516646-ec27055f09ddd37d9bfda39a5';
-const image = 'image_type=photo';
-const orientation = 'orientation=horizontal';
-const safesearch = 'safesearch=true';
+const BASE_URL = new URL('https://pixabay.com/api/?');
+const KEY = '41516646-ec27055f09ddd37d9bfda39a5';
 
-const containerDestroy = document.querySelectorAll('.gallery.gallery-item');
-for (const destroy of containerDestroy) {
-    console.log(destroy);
-}
-
-const from = document.querySelector('.form');
+const formSearch = document.querySelector('.form');
 const listGallery = document.querySelector('.gallery');
 const body = document.querySelector('body');
 
-from.addEventListener('submit', event => {
+formSearch.addEventListener('submit', event => {
     event.preventDefault();
-    handleClickDestroy();
+
     body.insertAdjacentHTML('beforeend', '<span class="loader"></span>');
     const resultsSearch = event.target.elements.search.value;
 
-    fetch(
-        `${BASE_URL}?${key}&q=${resultsSearch}&${image}&${orientation}&${safesearch}`
-    )
+    const searchParams = new URLSearchParams({
+        key: KEY,
+        q: resultsSearch,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: 'true',
+    });
+    const url = `${BASE_URL}${searchParams}`;
+
+    fetch(url)
         .then(response => {
             if (!response.ok) {
                 throw new Error(response.status);
@@ -35,62 +34,65 @@ from.addEventListener('submit', event => {
         })
 
         .then(result => {
-            const dataFormImage = result.hits;
-            if (!dataFormImage.length) {
-                iziToast.error({
-                    message: `❌ Sorry, there are no images matching your search query. Please, try again!`,
-                    icon: '',
-
-                    position: 'topRight',
-                });
-            }
-            const galleryMarkup = dataFormImage.reduce(
-                (
-                    html,
-                    {
-                        webformatURL,
-                        largeImageURL,
-                        tags,
-                        likes,
-                        views,
-                        comments,
-                        downloads,
-                    }
-                ) =>
-                    html +
-                    `<li class="gallery-item">
-            <a class="gallery-link" href="${largeImageURL}">
-                <img class="gallery-image" src="${webformatURL}" alt="${tags}"/>
-            </a>
-            <ul class="characteristics-list">
-            <li class="characteristics"><span class="characteristics-titel">Likes</span> <span>${likes}</span></li>
-            <li class="characteristics"><span class="characteristics-titel">Views</span> <span>${views}</span></li>
-            <li class="characteristics"><span class="characteristics-titel">Comments</span> <span>${comments}</span></li>
-            <li class="characteristics"><span class="characteristics-titel">Downloads</span> <span>${downloads}</span></li>
-        </ul>
-            
-        </li>`,
-                ''
-            );
-            listGallery.insertAdjacentHTML('beforeend', galleryMarkup);
-            let gallery = new SimpleLightbox('.gallery a', {
-                captionsData: 'alt',
-                captionDelay: 250,
-                captionPosition: 'bottom',
-            });
-
-            gallery.refresh();
-            const loaderDestroy = document.querySelector('.loader');
-            loaderDestroy.remove();
+            formMarkupCreating(result);
         })
         .catch(error => {
-            console.log(error.message);
+            modalIziToast(error);
         });
 });
 
-function handleClickDestroy() {
-    const containerDestroy = document.querySelectorAll('.gallery-item');
-    for (const destroy of containerDestroy) {
-        destroy.remove();
+function formMarkupCreating(result) {
+    const dataFormImage = result.hits;
+    if (!dataFormImage.length) {
+        modalIziToast(error);
     }
+    const galleryMarkup = dataFormImage.reduce(
+        (
+            html,
+            {
+                webformatURL,
+                largeImageURL,
+                tags,
+                likes,
+                views,
+                comments,
+                downloads,
+            }
+        ) =>
+            html +
+            `<li class="gallery-item">
+    <a class="gallery-link" href="${largeImageURL}">
+        <img class="gallery-image" src="${webformatURL}" alt="${tags}"/>
+    </a>
+    <ul class="characteristics-list">
+    <li class="characteristics"><span class="characteristics-titel">Likes</span> <span>${likes}</span></li>
+    <li class="characteristics"><span class="characteristics-titel">Views</span> <span>${views}</span></li>
+    <li class="characteristics"><span class="characteristics-titel">Comments</span> <span>${comments}</span></li>
+    <li class="characteristics"><span class="characteristics-titel">Downloads</span> <span>${downloads}</span></li>
+</ul>
+    
+</li>`,
+        ''
+    );
+
+    listGallery.innerHTML = galleryMarkup;
+
+    let gallery = new SimpleLightbox('.gallery a', {
+        captionsData: 'alt',
+        captionDelay: 250,
+        captionPosition: 'bottom',
+    });
+
+    gallery.refresh();
+    const loaderDestroy = document.querySelector('.loader');
+    loaderDestroy.remove();
+}
+
+function modalIziToast() {
+    iziToast.error({
+        message: `❌ Sorry, there are no images matching your search query. Please, try again!`,
+        icon: '',
+
+        position: 'topRight',
+    });
 }
